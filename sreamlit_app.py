b96f34544c3f21f3614195e793204df1c229b49e
zenking2 <141273285+zenking2@users.noreply.github.com>
2023-08-02 16:06:59 -0500

    Update sreamlit_app.py
    Updated the Title
    Added Breakfast Menu

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
-streamlit.title('First line of code in Github')
+streamlit.title('My Parents New Healthy Dinner')
+
+Omega 3 & Blueberry Oatmeal
+
+Kale, Spinach & Rocket Smoothie
+
+Half-Boiled Free-Range Egg
