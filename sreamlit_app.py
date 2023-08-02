a89e4f9dd11c72cc98d7c25c36e928acb279492f
zenking2 <141273285+zenking2@users.noreply.github.com>
2023-08-02 16:09:43 -0500

    Update sreamlit_app.py
    Added more items to the Breakfast Menu

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
-Omega 3 & Blueberry Oatmeal
-
-Kale, Spinach & Rocket Smoothie
-
-Half-Boiled Free-Range Egg
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Half-Boiled Free-Range Egg')
