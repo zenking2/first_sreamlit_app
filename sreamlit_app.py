6f0b2220b942930eccb5297667cb5479be1dfdfe
zenking2 <141273285+zenking2@users.noreply.github.com>
2023-08-02 17:01:22 -0500

    Update sreamlit_app.py
    Fruits Table

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -10,4 +10,6 @@ streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 streamlit.header('Importing CSV File from S3 using Pandas Library')
+streamlit.text('Fruits Size Table')
+
 import pandas
