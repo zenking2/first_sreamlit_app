628a525cf422c1c5433934ce530759e7f728f6ad
zenking2 <141273285+zenking2@users.noreply.github.com>
2023-08-06 16:07:30 -0500

    Update sreamlit_app.py
    Fix

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -15,7 +15,7 @@ streamlit.text('Fruits Size Table')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_first_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
