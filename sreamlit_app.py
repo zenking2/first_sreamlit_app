03fb1572341dfd6625328687a1f78b061265597a
zenking2 <141273285+zenking2@users.noreply.github.com>
2023-08-06 14:08:56 -0500

    Update sreamlit_app.py
    Read File

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -13,3 +13,5 @@ streamlit.header('Importing CSV File from S3 using Pandas Library')
 streamlit.text('Fruits Size Table')
 
 import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
