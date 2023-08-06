b53f23fad5b9f95c9e30c85660b38380857d6391
zenking2 <141273285+zenking2@users.noreply.github.com>
2023-08-06 16:09:01 -0500

    Update sreamlit_app.py
    Fix

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -19,4 +19,4 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-streamlit.dataframes(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
