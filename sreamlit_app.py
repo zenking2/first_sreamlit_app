3f0c77ff85acd4f7a954c453cd4cb78a1b339124
zenking2 <141273285+zenking2@users.noreply.github.com>
2023-08-08 10:33:56 -0500

    Update sreamlit_app.py

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -27,4 +27,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
